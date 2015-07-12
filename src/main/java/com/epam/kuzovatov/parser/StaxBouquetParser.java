bc29414d36bc0919f90047387b7497166f24fe10
Pavel <pavel.kuzovatov@gmail.com>
2015-07-13 02:52:29 +0600

    refactor stax

--- src/main/java/com/epam/kuzovatov/parser/StaxBouquetParser.java
+++ src/main/java/com/epam/kuzovatov/parser/StaxBouquetParser.java
@@ -37,55 +37,6 @@ public class StaxBouquetParser implements ComponentParser {
                         EndElement endElement = event.asEndElement();
                         String endElementName = endElement.getName().getLocalPart();
                         System.out.println(endElementName);
-
-
-//                        if (qName.equalsIgnoreCase("student")) {
-//                            System.out.println("Start Element : student");
-//                            Iterator<Attribute> attributes = startElement.getAttributes();
-//                            String rollNo = attributes.next().getValue();
-//                            System.out.println("Roll No : " + rollNo);
-//                        } else if (qName.equalsIgnoreCase("firstname")) {
-//                            bFirstName = true;
-//                        } else if (qName.equalsIgnoreCase("lastname")) {
-//                            bLastName = true;
-//                        } else if (qName.equalsIgnoreCase("nickname")) {
-//                            bNickName = true;
-//                        }
-//                        else if (qName.equalsIgnoreCase("marks")) {
-//                            bMarks = true;
-//                        }
-//                        break;
-//                    case XMLStreamConstants.CHARACTERS:
-//                        Characters characters = event.asCharacters();
-//                        if(bFirstName){
-//                            System.out.println("First Name: "
-//                                    + characters.getData());
-//                            bFirstName = false;
-//                        }
-//                        if(bLastName){
-//                            System.out.println("Last Name: "
-//                                    + characters.getData());
-//                            bLastName = false;
-//                        }
-//                        if(bNickName){
-//                            System.out.println("Nick Name: "
-//                                    + characters.getData());
-//                            bNickName = false;
-//                        }
-//                        if(bMarks){
-//                            System.out.println("Marks: "
-//                                    + characters.getData());
-//                            bMarks = false;
-//                        }
-//                        break;
-//                    case  XMLStreamConstants.END_ELEMENT:
-//                        EndElement endElement = event.asEndElement();
-//                        if(endElement.getName().getLocalPart().equalsIgnoreCase("student")){
-//                            System.out.println("End Element : student");
-//                            System.out.println();
-//                        }
-//                        break;
-
                 }
             }
         } catch (XMLStreamException e) {
